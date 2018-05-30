af66e0703c02c207f6f6fc8922263213f90b62ff
rye@woolworths.com.au <rye@woolworths.com.au>
2018-05-30 16:13:18 +1000

    first commit

---
+++ app/src/main/java/com/ye/r/receiptsafe/ReceiptListActivity.kt
@@ -0,0 +1,62 @@
+package com.ye.r.receiptsafe
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_receipt_list.*
+import android.provider.MediaStore
+import android.content.Intent
+import android.graphics.Bitmap
+import kotlinx.android.synthetic.main.content_receipt_list.*
+
+
+class ReceiptListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receipt_list)
+        setSupportActionBar(toolbar)
+
+
+        fab.setOnClickListener { view ->
+            dispatchTakePictureIntent()
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+    val REQUEST_IMAGE_CAPTURE = 1
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val extras = data.extras
+            val imageBitmap = extras!!.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_receipt_list, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
