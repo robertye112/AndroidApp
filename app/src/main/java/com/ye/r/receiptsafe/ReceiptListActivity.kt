c85efeed529cfef1568504cfe21aafc447a49297
rye@woolworths.com.au <rye@woolworths.com.au>
2018-06-08 16:59:17 +1000

    going home friday

--- app/src/main/java/com/ye/r/receiptsafe/ReceiptListActivity.kt
+++ app/src/main/java/com/ye/r/receiptsafe/ReceiptListActivity.kt
@@ -10,7 +10,16 @@ import kotlinx.android.synthetic.main.activity_receipt_list.*
 import android.provider.MediaStore
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Environment
 import kotlinx.android.synthetic.main.content_receipt_list.*
+import android.os.Environment.DIRECTORY_PICTURES
+import android.os.Environment.getExternalStoragePublicDirectory
+import android.support.v4.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class ReceiptListActivity : AppCompatActivity() {
@@ -23,17 +32,42 @@ class ReceiptListActivity : AppCompatActivity() {
 
         fab.setOnClickListener { view ->
             dispatchTakePictureIntent()
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Replace with your own action1", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
 
     val REQUEST_IMAGE_CAPTURE = 1
     private fun dispatchTakePictureIntent() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (takePictureIntent.resolveActivity(packageManager) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            var photoFile: File? = null
+            try {
+                photoFile = createImageFile()
+                galleryAddPic()
+            } catch (ex: IOException) {
+                // Error occurred while creating the File
+                var test = ex.message
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                var photoURI = FileProvider.getUriForFile(this,
+                "com.example.android.fileprovider",
+                photoFile)
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
+    }
+
+    private fun galleryAddPic() {
+        val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+        val f = File(mCurrentPhotoPath)
+        val contentUri = Uri.fromFile(f)
+        mediaScanIntent.data = contentUri
+        this.sendBroadcast(mediaScanIntent)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
@@ -44,6 +78,27 @@ class ReceiptListActivity : AppCompatActivity() {
 
         }
     }
+
+    private var mCurrentPhotoPath: String = ""
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+                imageFileName, /* prefix */
+                ".jpg", /* suffix */
+                storageDir      /* directory */
+        )
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath()
+        return image
+    }
+
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_receipt_list, menu)
